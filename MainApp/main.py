b9515551d1598cc00649d94490b15562ce2c3042
mihneaclgr <mihneaclgr@gmail.com>
2026-03-02 14:46:12 +0200

    Generator function

--- MainApp/main.py
+++ MainApp/main.py
@@ -1,11 +1,11 @@
 # ======================================
-# Implementing a GA to create a schedule
+# GA implementation for school schedule creation
 # ======================================
 
 import numpy as np
 from random import shuffle
 
-#Five days in a week, from 8AM to 3PM
+# Five days in a week, from 8AM to 3PM
 DAYS = 5
 HOURS = 7
 
@@ -24,4 +24,26 @@ subjects = {
     "religion" : 1,
     "arts" : 1
 }
+
+# Random schedule generator based on available subjects and slots
+def generator(subjects : dict):
+    subject_list = []
+    for subject_name,subject_slots in subjects.items():
+        subject_list += [subject_name] * subject_slots
+
+    #Filling remaining slots with free time
+    subject_list += [None] * (DAYS*HOURS - len(subject_list))
+
+    #Randomization
+    shuffle(subject_list)
+
+    #Matrix configuration
+    schedule = np.ndarray((DAYS,HOURS), dtype = object)
+    schedule[:] = None
+    for day in range(DAYS):
+        for hour in range(HOURS):
+            schedule[day][hour] = subject_list[day*DAYS+hour]
+
+    return schedule
+
 
