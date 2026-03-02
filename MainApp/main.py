70d2ad1f86112a51d16e11b292ada2eb5e4c4110
mihneaclgr <mihneaclgr@gmail.com>
2026-03-02 18:21:31 +0200

    fix: generator faulty logic

--- MainApp/main.py
+++ MainApp/main.py
@@ -4,13 +4,14 @@
 
 import numpy as np
 from random import shuffle
+from random import randint
 
 # Five days in a week, from 8AM to 3PM
 DAYS = 5
 HOURS = 7
 
 #Subjects as name: slots/week
-subjects = {
+SUBJECTS = {
     "math" : 4,
     "programming" : 4,
     "romanian": 3,
@@ -31,18 +32,18 @@ def generator(subjects : dict):
     for subject_name,subject_slots in subjects.items():
         subject_list += [subject_name] * subject_slots
 
-    #Filling remaining slots with free time
+    # Filling remaining slots with free time
     subject_list += [None] * (DAYS*HOURS - len(subject_list))
 
-    #Randomization
+    # Randomization
     shuffle(subject_list)
 
-    #Matrix configuration
+    # Matrix configuration
     schedule = np.ndarray((DAYS,HOURS), dtype = object)
     schedule[:] = None
     for day in range(DAYS):
         for hour in range(HOURS):
-            schedule[day][hour] = subject_list[day*DAYS+hour]
+            schedule[day][hour] = subject_list[day*HOURS+hour]
 
     return schedule
 
@@ -82,10 +83,12 @@ def fitness(schedule : np.ndarray):
 
     return fitness
 
+# How many specimens we keep after selection
+keep_percent = 0.2
+
 # Selection process, top x% of all schedules given
 def selection (list_of_potential_schedules : list):
     best_specimens = []
-    keep_percent = 0.2
 
     # Ranking each potential schedule using our fitness function
     for schedule in list_of_potential_schedules:
@@ -102,23 +105,54 @@ def selection (list_of_potential_schedules : list):
 # Create a new children schedule based on two parent schedules
 def crossover (schedule1 : np.ndarray, schedule2: np.ndarray):
 
+    # How many subjects we keep from parent1
+    crossing_point = 0.5
+
     # Empty schedule
     child_schedule = np.ndarray((DAYS,HOURS), dtype = object)
     child_schedule[:] = None
 
-    # We take Mon-Tue-Wed from schedule1 and Thu-Fri from schedule2
-    for i in range(3):
-        child_schedule[i] = schedule1[i]
-    for i in range(2):
-        child_schedule[i+3] = schedule2[i+3]
+    # We keep part of the subjects from parent1 intact, and fill the remaining slots from parent2's remaining subjects
+    it_subjects = list(SUBJECTS.items())
+    kept_subjects = dict(it_subjects[:int(len(SUBJECTS) * crossing_point)])
+    unkept_subjects = dict(it_subjects[int(len(SUBJECTS) * crossing_point):])
+
+    kept_subjects[None] = DAYS*HOURS - sum([v for k,v in SUBJECTS.items()])                  # add the free slots from parent1
 
     return child_schedule
+
+# Using crossover randomly over our best schedules, we create our new generation
+def new_generation(best_schedules : list):
+
+    # We will store our new schedules as 'parent1_id - parent2_id' : child_schedule
+    new_schedules = dict()
+
+    # Getting back to our initial population count
+    while(len(new_schedules) < int(1.0/keep_percent) * len(best_schedules)):
+        # Pick 2 random parents
+        parent1_id = randint(0,len(best_schedules)-1)
+        parent2_id = randint(0,len(best_schedules)-1)
+
+        # Don't apply crossover over the same parent
+        if parent1_id == parent2_id:
+            continue
+
+        # Give each child an unique name
+        child_name = f"{parent1_id}-{parent2_id}"
+
+        # Add our new child to the list
+        if child_name not in new_schedules.keys():
+            new_schedules[child_name] = crossover(best_schedules[parent1_id][0], best_schedules[parent2_id][0])
+
+    # Return a list of all created children
+    return [child_schedule for child_name,child_schedule in new_schedules.items()]
 
 
 
 # =================== Testing =======================
-initial_schedules = [generator(subjects) for i in range(60)]
+initial_schedules = [generator(SUBJECTS) for i in range(60)]
 best_schedules = selection(initial_schedules)
+new_schedules = new_generation(best_schedules)
 print("Done!")
 
 
